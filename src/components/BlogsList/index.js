605b8eaca34a8a04559a06c5a4a5ea86532a8be5
abduls1234 <abdulsamad3125@gmail.com>
2023-12-08 14:50:17 +0000

    routing-using-react-router-part-2

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,48 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogData()
+  }
+
+  getBlogData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+
+    const data = await response.json()
+
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      topic: eachItem.topic,
+      avatarUrl: eachItem.avatar_url,
+      imageUrl: eachItem.image_url,
+      author: eachItem.author,
+    }))
+    this.setState({blogsData: formattedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" height={50} width={50} color="#00BFFF" />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
